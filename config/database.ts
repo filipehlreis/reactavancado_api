4db29b19c4454b3619f80690e3f4067544f966ed
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-09-27 23:20:34 -0300

    alterado database.js

--- config/database.ts
+++ config/database.ts
@@ -1,3 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "reactavancado"),
+      username: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "senha"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  }
+});
+
 // export default ({ env }) => ({
 //   connection: {
 //     client: 'postgres',
@@ -12,25 +27,25 @@
 //   },
 // });
 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'reactavancado'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'senha'),
-        ssl: {
-          rejectUnauthorized: false
-        }
-      },
-      options: {
-        ssl: true,
-      }
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'reactavancado'),
+//         username: env('DATABASE_USERNAME', 'strapi'),
+//         password: env('DATABASE_PASSWORD', 'senha'),
+//         ssl: {
+//           rejectUnauthorized: false
+//         }
+//       },
+//       options: {
+//         ssl: true,
+//       }
+//     },
+//   },
+// });
