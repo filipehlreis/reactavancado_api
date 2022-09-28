4c1f523137aec38b009e5c2914d2c922ff2e8a0f
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-09-27 21:49:29 -0300

    update database config file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,7 @@
-import parse = require('pg-connection-string').parse;
+const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
