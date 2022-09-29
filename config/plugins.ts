b21d74f9a420634e3295a51a2b85aad5ec72e29b
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-09-28 22:50:46 -0300

    adicionado a aws s3 para arquivos de fotos

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,6 @@
 // path: ./config/plugins.ts
 
-export default {
+module.exports = ({ env }) => ({
   //
   graphql: {
     config: {
@@ -14,4 +14,18 @@ export default {
       },
     },
   },
-};
+
+  upload: {
+    config: {
+      provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
+  },
+});
