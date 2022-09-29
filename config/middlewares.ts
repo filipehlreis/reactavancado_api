8447f39aca8012a64d1a8f8fbe76f547750a4a9f
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-09-28 23:04:50 -0300

    middlewares.ts modificado para usar aws s3

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,31 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'reactavancado-api-filipe.s3.sa-east-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'reactavancado-api-filipe.s3.sa-east-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
