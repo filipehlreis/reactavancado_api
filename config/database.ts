73d371ddbc28844f32f64e361b13e8eaf0d95e11
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-09-28 21:34:32 -0300

    alterado server.js e database.js e deletado env/production

--- config/database.ts
+++ config/database.ts
@@ -1,17 +1,64 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connection: {
-    client: "postgres",
+// const path = require("path");
+
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "reactavancado"),
-      username: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "senha"),
-      schema: env("DATABASE_SCHEMA", "public"),
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        schema: env("DATABASE_SCHEMA", "public"), // Not required
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        },
+      },
+      debug: false,
     },
-  }
-});
+  };
+};
+
+// Use this configuration for an SQLite database on your machine.
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "sqlite",
+//     connection: {
+//       filename: path.join(
+//         __dirname,
+//         "..",
+//         env("DATABASE_FILENAME", ".tmp/data.db")
+//       ),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+
+
+
+
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "localhost"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "reactavancado"),
+//       username: env("DATABASE_USERNAME", "strapi"),
+//       password: env("DATABASE_PASSWORD", "senha"),
+//       schema: env("DATABASE_SCHEMA", "public"),
+//     },
+//   }
+// });
 
 // export default ({ env }) => ({
 //   connection: {
